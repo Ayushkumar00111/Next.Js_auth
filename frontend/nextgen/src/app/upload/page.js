977f71c9ef29b8bb34cc67cfd946857e7b0d707e
Ayush Kumar <ayushkumar42342@gmail.com>
2026-03-27 19:02:30 +0530

    27 march

--- frontend/nextgen/src/app/upload/page.js
+++ frontend/nextgen/src/app/upload/page.js
@@ -2,14 +2,26 @@
 import { useState } from "react"
 export default function upload(){
     const [data , setdata]=useState();
-    const fun =(e)=>{
+    const fun = async(e)=>{
       
-       console.log(data)
+  e.preventDefault();
+        if (!data) {
+            alert("Please select a file");
+            return;
+        }
+    
+
+       const form = new FormData();
+       form.set("file",data)
+       const result = await fetch("http://localhost:3000/upload/uploadapi",{
+        method:"Post",
+        body: form
+       })
     }
     return(
         <div>
             <h1> add files</h1>
-            <form action={fun}>
+            <form onSubmit={fun}>
    <input type="file" onChange={(e)=>{setdata(e.target.files?.[0])}}/>
             <button >ADD photo / files</button>
             </form>
